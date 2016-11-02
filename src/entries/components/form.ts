e9033d1dab894baaa47fda723df8c27e095f62eb
Robin Wenglewski <robin@wenglewski.de>
2016-11-02 15:04:50 +0100

    avoid cached entity on edit-entry-page

--- src/entries/components/form.ts
+++ src/entries/components/form.ts
@@ -1,7 +1,7 @@
 import { Component, Input, EventEmitter, Output } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
-import { DefaultsTo } from '@rweng/util';
-import { constant, merge, negate, isEmpty } from 'lodash';
+import { AfterSet } from '@rweng/util';
+import { merge, negate, isEmpty } from 'lodash';
 import { Entry } from '../models';
 
 
@@ -11,7 +11,7 @@ import { Entry } from '../models';
 })
 export class EntryForm {
     @Input()
-    @DefaultsTo(constant({ datetime: (new Date()).toISOString() }))
+    @AfterSet((i) => i.resetForm())
     entry: Entry;
 
     @Output()
@@ -29,17 +29,22 @@ export class EntryForm {
     ngOnInit() {
         // TODO unsub
         this.formGroup.valueChanges.subscribe((v: { content: string, tags: string }) => {
-            this.entryChange.emit(merge(this.entry, {
-                content: v.content,
-                tags: v.tags && v.tags.split(',').map(t => t.trim()).filter(negate(isEmpty))
-            }));
+            this.entryChange.emit(merge(
+                { datetime: (new Date()).toISOString() },
+                this.entry || {},
+                {
+                    content: v.content,
+                    tags: v.tags && v.tags.split(',').map(t => t.trim()).filter(negate(isEmpty))
+                }));
         });
     }
 
-    ngOnChanges() {
-        this.formGroup.reset({
+    resetForm() {
+        const state = this.entry ? {
             content: this.entry.content,
             tags: this.entry.tags && this.entry.tags.join(',')
-        });
+        } : undefined;
+
+        this.formGroup.reset(state);
     }
 }
