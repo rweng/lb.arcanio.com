8ccb3311d13009d6c613ed92af061159d192dd08
Robin Wenglewski <robin@wenglewski.de>
2016-11-15 15:01:58 +0100

    add notification and redirect to settings page

--- src/app/pages/settings.ts
+++ src/app/pages/settings.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { NotificationService } from '@rweng/ng2-notifications';
 
 import { SyncService } from '../services';
 
@@ -12,11 +14,17 @@ export class SettingsPage {
         syncUrl: new FormControl(this.syncService.url, Validators.required)
     });
 
-    constructor(protected syncService: SyncService) { }
+    constructor(
+        private ns: NotificationService,
+        private router: Router,
+        private syncService: SyncService
+    ) { }
 
     save() {
         this.syncService.url = this.formData.syncUrl;
         this.syncService.connect();
+        this.ns.push('Sync Url saved');
+        this.router.navigate(['/']);
     }
 
     private get formData() {
